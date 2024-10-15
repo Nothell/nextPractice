fc0b397b7f56938ee7865afac5ce0ff2546a120c
Noel Soans <noelsoans57@gmail.com>
2024-10-14 20:24:40 -0400

    Initial commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { getDisplayName } from 'next/dist/shared/lib/utils';
+import {Inter} from 'next/font/google';
+export const inter = Inter({subsets: ['latin']})
+
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({weight:['400', '700'], subsets:['latin']})
+
